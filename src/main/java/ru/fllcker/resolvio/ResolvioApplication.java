c6de662097a7e3859b546a59d518dba0ce275932
jxwide <jxwide@gmail.com>
2023-04-19 22:34:05 +0300

    init

---
+++ src/main/java/ru/fllcker/resolvio/ResolvioApplication.java
@@ -0,0 +1,13 @@
+package ru.fllcker.resolvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolvioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/fllcker/resolvio/ResolvioApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fllcker.resolvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
